ef285fb3014a8f0d001c4162fd4a51661875b6a6
Juani <juani@2sy142.rpbaad.gob.ar>
2018-04-12 11:49:22 -0300

    Adapt to new ML pages

--- busqueda.py
+++ busqueda.py
@@ -5,23 +5,30 @@ cont = 1
 prom = 0
 acum = 0
 precio = 0
-for line in entrada:
-	e = line
 
-	i = e.find('ch-price')
+#For each line in file "entrada"
+for line in entrada:
+	#Put the line in variable "e"
+	e = line
+	
+	#Find a substr that contain 'price__fraction' and get the index in the line (i=initial position of the substr in the line)
+	i = e.find('price__fraction')
+	
+	#Put substr 'index + 25 follows chars' in the "s" variable
 	s = e[i : i + 25]
 	
 	while (i != -1):
 		if (len(s) != 0):
-			print ("s: " + s)
+			print ("s:" + s)
 			cont = cont+1
-			precio = s[12 : 12 + 7]
-			print ("precio: " + precio)
+			#Get the price of the substr in 's'
+			precio = s[18 : 18 + 7]
+			print ("precio:" + precio)
 			precio = eval(precio)
 			acum = acum + precio
 			salida.write(s + '\n') 
 			e = e[ (i + len(s)) : len(e)]
-			i = e.find('ch-price')
+			i = e.find('price__fraction')
 			s = e[ i : i + 25]
 		else:
 			i = -1
